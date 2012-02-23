444b6ad3dc90937ac5be0c5a46118e1a158e5d57
Irina <ircha22@gmail.com>
2012-02-24 00:21:05 +0700

    update posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   validates :login,    :presence => true, :uniqueness => true
   validates :email,    :presence => true, :uniqueness => true
 
-  has_many :posts, dependent => :destroy
+  has_many :posts, :dependent => :destroy
 end
